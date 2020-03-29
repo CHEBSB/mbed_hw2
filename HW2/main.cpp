74906d97c21c17964e5e9ccdec8095ccb8beefdf
107061110 <chebsb229@gmail.com>
2020-03-29 21:22:01 +0800

    debugging, redLED an't turn on

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -4,18 +4,18 @@
 
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
 unsigned char table[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
-DigitalIn  Switch(SW3);
+InterruptIn  Switch(SW3);
 DigitalOut redLED(LED1);
 DigitalOut greenLED(LED2);
 AnalogOut Aout(DAC0_OUT);
 AnalogIn Ain(A0);
 
-void sevenSeg(double freq);	// number of digits
+void sevenSeg();	// number of digits
 int CountFreq(double *A);	// how many samples to repeat
 int pwrOfTen(int pwr);	// calculate 10^pwr
 int NumOfDigit(double x);	/* calculate number of digit
 of the number's nearest integer */
-
+int *d;
 int main() {
 	double Data[40];	// store data to calculate frequency
 	double freq;		// the desired frequency
@@ -26,34 +26,30 @@ int main() {
 	}
 	freq = fs / CountFreq(Data);
 
+	/* build an array of each digit to display */
+	int n = NumOfDigit(freq), f = freq;	/* define f 
+	//so that we can modify f without concerning freq*/
+	d = new int[n];
+	for (int i = n - 1; i >= 0; i--) {
+		d[n - 1 - i] = (int)f / pwrOfTen(i);
+		f = f - d[i] * pwrOfTen(i);
+		if (i == 0 && f >= 0.5)  d[n - 1 - i]++;
+	}
+	Switch.rise(&sevenSeg);
 	while (1) {
-		if (Switch == 1) {
-			greenLED = 0;
-			redLED = 1;
-			sevenSeg(freq);
-		}
-		else {
-			redLED = 0;
-			greenLED = 1;
-		}
+		redLED = 0;
+		greenLED = 1;
 	}
 }
 
-void sevenSeg(double freq)
+void sevenSeg()
 {
-	/* build an array of each digit to display */
-	int n = NumOfDigit(freq);
-	int d[n];
-	for (int i = n - 1; i >= 0; i--) {
-		d[n - 1 - i] = (int)freq / pwrOfTen(i);
-		freq = freq - d[i] * pwrOfTen(i);
-		if (i == 0 && freq >= 0.5)  d[n - 1 - i]++;
-	}
-
-	for (int t = 0; Switch == 1; t++) {
-		if (t == n - 1) {	// the last digit
+	greenLED = 0;
+	redLED = 1;
+	for (int t = 0; ; t++) {
+		if (t == sizeof(d) - 1) {	// the last digit
 			display = table[d[t]] + 0x80;	// add decimal point
-			t -= n;	// prepare for restart the cycle
+			t -= sizeof(d);	// prepare for restart the cycle
 		}
 		else
 			display = table[d[t]];
