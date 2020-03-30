69156523a60c87b4133782bef6dae6d199abdbcd
107061110 <chebsb229@gmail.com>
2020-03-30 22:07:25 +0800

    idk how it works. But it works!

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 
+Serial pc( USBTX, USBRX );
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
 char table[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 DigitalIn  Switch(SW3);
@@ -14,23 +15,26 @@ int CountPeriod(double *A, int size); // how many samples to repeat
 int pwrOfTen(int pwr);	// calculate 10^pwr
 int NumOfDigit(int n);	/* calculate number of digits
 of the nearest integer to frequency */
+void setAout();
 
 int main() {
-	double Data[40];	// store data to calculate frequency
+	double Data[100];	// store data to calculate frequency
 	int n;		// number of digits of freq
 
-	for (int i = 0; i < 40; i++) {
+	for (int i = 0; i < 100; i++) {
 		Data[i] = Ain;	// sampling
 		wait(1 / fs);
 	}
-	if (CountPeriod(Data, 40) == 0) {	// which shouldn't be 0
-		redLED = 1;	// light up redLED, showing error
-		while (1) {}
-	}
-	freq = fs / CountPeriod(Data, 40);
+	for (int i = 0; i < 100; i++){
+	// output data to Python for plotting
+   	 	pc.printf("%1.3lf\r\n", Data[i]);
+   	 	wait(0.01);
+  	}	
+	freq = fs / CountPeriod(Data, 100);
 
-	int f = (int)freq;	// integer part of freq
-	if (freq - f >= 0.5) f++;	// to the nearest integer
+	int f = (int)freq;	/* integer part of freq
+	fs is interger (400), CountPeriod return integer
+	-> actually f = freq */
 	n = NumOfDigit(f);
 	d = new int[n];
 	for (int i = n - 1; i >= 0; i--) {
@@ -39,10 +43,10 @@ int main() {
 	}
 
 	while (1) {
-		if (Switch == 1) {
-			greenLED = 0;
-			redLED = 1;
-			for (int t = 0; Switch == 1; t++) {
+		redLED = Switch;
+		greenLED = !redLED;
+		if (Switch == 0) {
+			for (int t = 0; Switch == 0; t++) {
 				if (t == n - 1) {	// the last digit
 					// add decimal point
 					display = table[d[t]] + 0x80;	
@@ -50,17 +54,17 @@ int main() {
 				}
 				else
 					display = table[d[t]];
-				wait(0.2);
+				wait(0.3);
 			}
 		}
 		else {
-			redLED = 0;
-			greenLED = 1;
+			display = 0x00;
 			for (double t = 0; t < (1 / freq); t += (0.05 / freq)) {
 				Aout = 0.5 + 0.5 * sin(2 * 3.14159 * freq * t);
 				wait(0.05 / freq);
-			}
+			}	
 		}
+		
 	}
 }
 
@@ -75,12 +79,11 @@ int CountPeriod(double *A, int size)
 	 we can't assure its correctness ) */
 		flag = 1;	// for each i, first set flag = 1
 		for (int j = 0; j < size - i; j++) 
-			if ((A[j] - A[j + i] > 0.2) || (A[j + i] - A[j] > 0.2)) 
+			if ((A[j] - A[j + i] > 0.1) || (A[j + i] - A[j] > 0.1)) 
 			// if any 2 elements do not match the period
 				flag = 0;
 	}
-
-	if (i > size / 2)	return 0;	// unperiodic
+	if (i > size / 2)	return 0;	// aperiodic
 	return i - 1;
 }
 int pwrOfTen(int pwr)
