4bebdf3f21b0c6da7380521ccb8b6e6b11b9ab6c
107061110 <chebsb229@gmail.com>
2020-03-30 16:22:42 +0800

    A great progress made! can retrieve this version if necessary

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,89 +1,83 @@
 #include "mbed.h"
-#define abs(a) (a >= 0? a: (-1 * a))	// absolute value
 
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
-unsigned char table[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
+char table[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 DigitalIn  Switch(SW3);
-DigitalOut redLED(LED1);
-DigitalOut greenLED(LED2);
+DigitalOut redLED(LED1), greenLED(LED2);
 AnalogOut Aout(DAC0_OUT);
 AnalogIn Ain(A0);
-const double fs = 400;	// 400 hertz sampling frequency
+const double fs = 400.0;	// 400hz sampling frequency
 double freq;		// frequency of input sine wave
-int *d; // array for digits of the frequency
+int *d;	// array for digits of the nearest integer to freq
 
-void sevenSeg();	// number of digits
-int CountPeriod(double *A);	// how many samples to repeat
+int CountPeriod(double *A, int size); // how many samples to repeat
 int pwrOfTen(int pwr);	// calculate 10^pwr
-int NumOfDigit(double x);	/* calculate number of digits
-of the nearest integer to frequency*/
-void showWave(); // output the waveform to picoscope
+int NumOfDigit(int n);	/* calculate number of digits
+of the nearest integer to frequency */
 
 int main() {
 	double Data[40];	// store data to calculate frequency
 	int n;		// number of digits of freq
-	double f; // a copy of freq for modification
 
-	for (int t = 0; t < 40; t += 1) {
-		Data[t] = Ain;	// sampling
+	for (int i = 0; i < 40; i++) {
+		Data[i] = Ain;	// sampling
 		wait(1 / fs);
 	}
-	if (CountPeriod(Data) == 0) {
-		redLED = 1;	// forever lighting up redLED
-		while(1) {}
+	if (CountPeriod(Data, 40) == 0) {	// which shouldn't be 0
+		redLED = 1;
+		while (1) {}
 	}
-	freq = fs / CountPeriod(Data);
+	freq = fs / CountPeriod(Data, 40);
 
 	/* build an array of each digit to display */
-	n = NumOfDigit(freq);
-	f = freq;	
+	int f = (int)freq;	// integer part of freq to modify
+	if (freq - f >= 0.5) f++;	
+	n = NumOfDigit(f);
 	d = new int[n];
 	for (int i = n - 1; i >= 0; i--) {
-		d[n - 1 - i] = (int)f / pwrOfTen(i);
+		d[n - 1 - i] = f / pwrOfTen(i);
 		f = f - d[n - 1 - i] * pwrOfTen(i);
-		if (i == 0 && f >= 0.5)  d[n - 1 - i]++;
 	}
 
 	while (1) {
 		if (Switch == 1) {
 			greenLED = 0;
 			redLED = 1;
-			sevenSeg();
+			for (int t = 0; Switch == 1; t++) {
+				if (t == n - 1) {	// the last digit
+					// add decimal point
+					display = table[d[t]] + 0x80;	
+					t -= n;	// restart the cycle
+				}
+				else
+					display = table[d[t]];
+				wait(0.25);
+			}
 		}
 		else {
 			redLED = 0;
 			greenLED = 1;
-			showWave();
+			for (double t = 0; t < (1 / freq); t += (0.1 / freq)) {
+				Aout = sin(2 * 3.14159 * freq * t);
+				wait(0.1 / freq);
+			}
 		}
 	}
 }
 
-void sevenSeg()
-{
-	int n = sizeof(d);
-	for (int t = 0; Switch == 1; t++) {
-		if (t == n - 1) {	// the last digit
-			display = table[d[t]] + 0x80;	// add decimal point
-			t -= n;	// prepare for restart the cycle
-		}
-		else
-			display = table[d[t]];
-		wait(0.3);
-	}
-}
-int CountPeriod(double *A)
+int CountPeriod(double *A, int size)
 {
 	bool flag = 0;
-	unsigned int i;
+	int i;
 
-	for (i = 1; flag == 0 && i < sizeof(A); i++) {
+	for (i = 1; flag == 0 && i <= size / 2; i++) {
 		flag = 1;
-		for (unsigned int j = 0; j < sizeof(A) - i; j++) {
-			if (abs(A[j] - A[j + i]) > 0.2) flag = 0;
-		}
+		for (int j = 0; j < size - i; j++) 
+			if ((A[j] - A[j + i] > 0.2) || (A[j + i] - A[j] > 0.2)) 
+				flag = 0;
 	}
 
-	if (i == sizeof(A))	return 0;	// unperiodic
+	if (i > size / 2)	return 0;	// unperiodic
 	return i - 1;
 }
 int pwrOfTen(int pwr)
@@ -92,16 +86,9 @@ int pwrOfTen(int pwr)
 	while (pwr--) output *= 10;
 	return output;
 }
-int NumOfDigit(double x)
+int NumOfDigit(int n)
 {
-	int i;
-	for (i = 0; x >= pwrOfTen(i); i++);
+	int i = 0;
+	for (; n >= pwrOfTen(i); i++);
 	return i;
-}
-void showWave()
-{
-	for (double t = 0; t < 1 / freq; t += 0.1 / freq) {
-		Aout = 1.0 + 1.0 * sin(2 * 3.14159 * freq * t);
-		wait(0.1 / freq);
-	}
 }
